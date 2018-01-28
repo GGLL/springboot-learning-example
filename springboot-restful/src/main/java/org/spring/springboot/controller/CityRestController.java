1fb7582a8c9e65e21dae74c0db03bf3bbdd43794
GGLL <944600893@qq.com>
2018-01-28 17:07:39 +0800

    整合Swagger2

--- springboot-restful/src/main/java/org/spring/springboot/controller/CityRestController.java
+++ springboot-restful/src/main/java/org/spring/springboot/controller/CityRestController.java
@@ -1,9 +1,9 @@
 package org.spring.springboot.controller;
 
+import io.swagger.annotations.ApiOperation;
 import org.spring.springboot.domain.City;
 import org.spring.springboot.service.CityService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,32 +14,35 @@ import java.util.List;
  * Created by bysocket on 07/02/2017.
  */
 @RestController
+@RequestMapping(value = "/api/city")
 public class CityRestController {
 
     @Autowired
     private CityService cityService;
 
-    @RequestMapping(value = "/api/city/{id}", method = RequestMethod.GET)
+    @RequestMapping(value = "{id}", method = RequestMethod.GET)
     public City findOneCity(@PathVariable("id") Long id) {
         return cityService.findCityById(id);
     }
 
-    @RequestMapping(value = "/api/city", method = RequestMethod.GET)
+    @ApiOperation(value="获取城市列表", notes="")
+    @RequestMapping(value = "", method = RequestMethod.GET)
     public List<City> findAllCity() {
         return cityService.findAllCity();
     }
 
-    @RequestMapping(value = "/api/city", method = RequestMethod.POST)
+    @ApiOperation(value="创建城市", notes="根据city对象创建城市")
+    @RequestMapping(value = "", method = RequestMethod.POST)
     public void createCity(@RequestBody City city) {
         cityService.saveCity(city);
     }
 
-    @RequestMapping(value = "/api/city", method = RequestMethod.PUT)
+    @RequestMapping(value = "", method = RequestMethod.PUT)
     public void modifyCity(@RequestBody City city) {
         cityService.updateCity(city);
     }
 
-    @RequestMapping(value = "/api/city/{id}", method = RequestMethod.DELETE)
+    @RequestMapping(value = "{id}", method = RequestMethod.DELETE)
     public void modifyCity(@PathVariable("id") Long id) {
         cityService.deleteCity(id);
     }
